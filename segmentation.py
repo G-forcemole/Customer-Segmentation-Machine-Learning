9507411bd6c4112d77304cc6cc3010272b45fbe0
G-forcemole <gabedossantos2003@gmail.com>
2026-02-16 01:33:10 -0500

    Created requirements.txt & Streamlit app

---
+++ segmentation.py
@@ -0,0 +1,45 @@
+import streamlit as st 
+import pandas as pd 
+import numpy as np 
+import joblib 
+
+kmeans = joblib.load("kmeans_model.pkl")
+scaler = joblib.load("scaler.pkl")
+
+st.title("Customer Segmentation App")
+st.write("Enter customer details to predict the segment.")
+
+
+age = st.number_input("Age", min_value = 18, max_value = 100, value = 35)
+income = st.number_input("Income", min_value = 0, max_value = 200_000, value = 50_000)
+total_spending = st.number_input("Total Spending", min_value = 0, max_value = 5_000, value = 1_000)
+num_web_purchases = st.number_input("Number of Web Purchases", min_value = 0, max_value = 100, value = 10)
+num_store_purchases = st.number_input("Number of Store Purchases", min_value = 0, max_value = 100, value = 10)
+num_web_visits_month = st.number_input("Number of Web Visits per Month", min_value = 0, max_value = 50, value = 5)
+recency = st.number_input("Recency (days since last purchase)", min_value = 0, max_value = 365, value = 30)
+
+input_data = pd.DataFrame({
+    "Age": [age],
+    "Income": [income], 
+    "Total_Spending": [total_spending], 
+    "NumWebPurchases": [num_web_purchases], 
+    "NumStorePurchases": [num_store_purchases], 
+    "NumWebVisitsMonth": [num_web_visits_month], 
+    "Recency": [recency]}
+)
+
+input_scaled = scaler.transform(input_data)
+
+if st.button("Predict Segment"):
+
+    cluster = kmeans.predict(input_scaled)[0]
+
+    st.success(f"The Predicted segment: Cluster {cluster}")
+
+  
+
+    
+
+
+
+
